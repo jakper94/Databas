7b9fea989c61292d4d9dac0eb1e306f5ae881629
jakob <id16jpn@cs.umu.se>
2019-03-12 12:29:24 +0100

    p

--- idaginatt/idaginatt/WebApplication1/WebApplication1/Database/dbo.Table.sql
+++ idaginatt/idaginatt/WebApplication1/WebApplication1/Database/dbo.Table.sql
@@ -1,4 +1,12 @@
 ﻿CREATE TABLE [dbo].[Tbl_User]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Us_UserName] NCHAR(10) NULL, 
+    [Us_FirstName] NCHAR(10) NULL, 
+    [Us_LastName] NCHAR(10) NULL, 
+    [Us_HasVoted] NCHAR(10) NULL, 
+    [Us_IsAdmin] NCHAR(10) NULL, 
+    [Us_Class] NCHAR(10) NULL, 
+    [Us_HasChangedPassword] NCHAR(10) NULL, 
 )
+
