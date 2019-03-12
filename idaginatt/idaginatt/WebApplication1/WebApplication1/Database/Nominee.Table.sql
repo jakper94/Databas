b5a2f41fe8f5fd0125fa4c229d7f274827faa8bb
jakob <id16jpn@cs.umu.se>
2019-03-12 13:32:09 +0100

    added the two other databases

---
+++ idaginatt/idaginatt/WebApplication1/WebApplication1/Database/Nominee.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tbl_Nominee]
+(
+	[Nom_Id] INT NOT NULL PRIMARY KEY, 
+    [Nom_FirstName] NCHAR(10) NULL, 
+    [Nom_LastName] NCHAR(10) NULL, 
+    [Nom_ImgLink] NCHAR(50) NULL
+)
---
+++ idaginatt/idaginatt/WebApplication1/WebApplication1/Database/Vote.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Tbl_Vote]
+(
+	[Vo_Id] INT NOT NULL PRIMARY KEY, 
+    [Vo_Motivation] NVARCHAR(100) NULL, 
+    [Vo_VotedOn] INT NULL
+)
