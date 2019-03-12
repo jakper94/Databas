604074f552038a8a15af1abf2672753716b76f43
jakob <id16jpn@cs.umu.se>
2019-03-12 12:08:10 +0100

    Added a datebase file

---
+++ idaginatt/idaginatt/WebApplication1/WebApplication1/Database/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Tbl_User].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] NCHAR(10) NOT NULL, 
+    [Password] NCHAR(30) NOT NULL, 
+    [FirstName] NCHAR(30) NULL, 
+    [LastName] NCHAR(30) NULL, 
+    [HasVoted] NCHAR(10) NULL, 
+    [Class] NCHAR(10) NULL, 
+    [IsAdmin] NCHAR(10) NULL
+)
