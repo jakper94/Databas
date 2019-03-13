0527af84f94ddace8f932c6659bad3d225a0db46
Alfred Persson <affee97@gmail.com>
2019-03-13 13:00:24 +0100

    Added user methods

--- idaginatt/idaginatt/WebApplication1/WebApplication1/Models/UserMethod.cs
+++ idaginatt/idaginatt/WebApplication1/WebApplication1/Models/UserMethod.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,67 @@ namespace WebApplication1.Models
 {
     public class UserMethod
     {
-        public UserMethod()
+        public UserMethod() {}
+
+        public int InsertUser(UserDetail ud, out string errormsg)
         {
-            
+            SqlConnection dbConnection = new SqlConnection();
+
+            dbConnection.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Idag_Inatt;Integrated Security=True";
+
+            String sqlString = "INSERT INTO Tbl_User (Us_UserName, Us_Password) VALUES (@User_UserName, @User_Password)";
+            SqlCommand dbCommand = new SqlCommand(sqlString, dbConnection);
+
+            dbCommand.Parameters.Add("User_UserName", SqlDbType.NChar,8).Value = ud.User_UserName;
+            dbCommand.Parameters.Add("User_Password", SqlDbType.NChar, 10).Value = ud.User_Password;
+
+            try
+            {
+                dbConnection.Open();
+                int i = 0;
+                i = dbCommand.ExecuteNonQuery();
+                if (i == 1) { errormsg = ""; }
+                else { errormsg = "User not added to database."; }
+                return (i);
+            }
+            catch (Exception e)
+            {
+                errormsg = e.Message;
+                return 0;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
+
+        public int LogIn(string userName, string password, out string errormsg)
+        {
+            SqlConnection dbConnection = new SqlConnection();
+
+            dbConnection.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Idag_Inatt;Integrated Security=True";
+
+            String sqlString = "SELECT FROM Tbl_User WHERE Us_UserName = @userName";
+            SqlCommand dbCommand = new SqlCommand(sqlString, dbConnection);
+
+            try
+            {
+                dbConnection.Open();
+                int i = 0;
+                i = dbCommand.ExecuteNonQuery();
+                if (i == 1) { errormsg = ""; }
+                else { errormsg = "User not added to database."; }
+                return (i);
+            }
+            catch (Exception e)
+            {
+                errormsg = e.Message;
+                return 0;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
         }
     }
 }
