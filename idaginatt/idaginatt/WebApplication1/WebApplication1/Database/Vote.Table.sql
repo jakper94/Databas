024ba29649ab87931b864bdc273d103729e48017
jakob <id16jpn@cs.umu.se>
2019-03-13 10:01:42 +0100

    nominee changed

--- idaginatt/idaginatt/WebApplication1/WebApplication1/Database/Vote.Table.sql
+++ idaginatt/idaginatt/WebApplication1/WebApplication1/Database/Vote.Table.sql
@@ -3,6 +3,6 @@
 	[Vo_Id] INT IDENTITY (1, 1) NOT NULL, 
     [Vo_Motivation] NVARCHAR(100) NULL, 
     [Vo_VotedOn] INT NOT NULL,
-	CONSTRAINT [Pk_Tbl_Id] PRIMARY KEY CLUSTERED ([Nom_Id] ASC),
+	CONSTRAINT [Pk_Tbl_Vote] PRIMARY KEY CLUSTERED ([Vo_Id] ASC),
 	CONSTRAINT [FK_Tbl_Nominee] FOREIGN KEY ([Vo_VotedOn]) REFERENCES [dbo].[Tbl_Nominee] ([Nom_Id])
 )
