fee54fee0bf10f65787032f2dfd32f3c1fbbd245
jakob <id16jpn@cs.umu.se>
2019-03-15 09:59:25 +0100

    added attending

---
+++ idaginatt/idaginatt/WebApplication1/WebApplication1/Database/Attending.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tbl_Attending]
+(
+	[At_Id] INT IDENTITY (1, 1) NOT NULL, 
+    [At_FoodPred] NVARCHAR(MAX) NULL
+	CONSTRAINT [Pk_Tbl_Attending] PRIMARY KEY CLUSTERED ([At_ID] ASC)
+
+)
