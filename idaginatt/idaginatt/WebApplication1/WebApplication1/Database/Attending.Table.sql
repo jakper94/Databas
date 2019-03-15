18226b39a8b00d230bf653004ce07773086a0ba8
jakob <id16jpn@cs.umu.se>
2019-03-15 10:00:26 +0100

    edited attending

--- idaginatt/idaginatt/WebApplication1/WebApplication1/Database/Attending.Table.sql
+++ idaginatt/idaginatt/WebApplication1/WebApplication1/Database/Attending.Table.sql
@@ -2,6 +2,6 @@
 (
 	[At_Id] INT IDENTITY (1, 1) NOT NULL, 
     [At_FoodPred] NVARCHAR(MAX) NULL
-	CONSTRAINT [Pk_Tbl_Attending] PRIMARY KEY CLUSTERED ([At_ID] ASC)
+	CONSTRAINT [Pk_Tbl_Attending] PRIMARY KEY CLUSTERED ([At_Id] ASC)
 
 )
